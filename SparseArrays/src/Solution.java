d73b30e14ab2a9015f9864bbc6b6ba70636ed257
Guilherme Canuto <ganuto@hotmail.com>
2025-02-06 23:46:10 -0300

    continue...

---
+++ SparseArrays/src/Solution.java
@@ -0,0 +1,23 @@
+package SparseArrays.src;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public List<Integer> matchingStrings(List<String> stringList, List<String> queries) {
+        Map<String, Integer> map = new LinkedHashMap<>();
+        for (String string : stringList) {
+            map.put(string, map.getOrDefault(string, 0) + 1);
+        }
+        ArrayList<Integer> result = new ArrayList<>();
+        for (String query : queries) {
+            result.add(map.getOrDefault(query, 0));
+        }
+        return result;
+    }
+
+}
+
+
