6d26517db5fc1349df088c9f01c86e97e13ee356
unknown <ganuto@hotmail.com>
2025-02-07 18:15:25 -0300

    HackerRank WeeklyPrepKit

---
+++ PlusMinus/src/Main.java
@@ -0,0 +1,10 @@
+package PlusMinus.src;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.plusMinus(List.of(-4, 3, -9, 0, 4, 1));
+    }
+}
