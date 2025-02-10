6681d1bdcbc77371b6fc5f5f5370dacfb06e3e91
unknown <ganuto@hotmail.com>
2025-02-10 19:14:16 -0300

    String Encode and Decode - Fastest algo

--- StringEncodeAndDecode/src/Solution.java
+++ StringEncodeAndDecode/src/Solution.java
@@ -2,7 +2,6 @@ package StringEncodeAndDecode.src;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 class Solution {
     private static final String LOOKUP = "0123456789#";
@@ -15,30 +14,46 @@ class Solution {
         return encodedString;
     }
 
-    // First successful attempt
+//    // First successful attempt
+//    public List<String> decode(String str) {
+//        char[] chars = str.toCharArray();
+//        List<String> decodedString = new ArrayList<>();
+//        List<String> stringList = new ArrayList<>(chars.length);
+//        for (char c : chars) {
+//            stringList.add(String.valueOf(c));
+//        }
+//        String wordSize = "";
+//        while (!stringList.isEmpty()) {
+//            if (!Objects.equals(stringList.get(0), "#")) {
+//                wordSize += stringList.remove(0);
+//            } else {
+//                int size = Integer.parseInt(wordSize);
+//                stringList.remove("#");
+//                String word = "";
+//                for (int j = 0; j < size; j++) {
+//                    word += stringList.remove(0);
+//                }
+//                wordSize = "";
+//                decodedString.add(word);
+//            }
+//        }
+//        return decodedString;
+//    }
+
+    // Fastest algo
     public List<String> decode(String str) {
-        char[] chars = str.toCharArray();
-        List<String> decodedString = new ArrayList<>();
-        List<String> stringList = new ArrayList<>(chars.length);
-        for (char c : chars) {
-            stringList.add(String.valueOf(c));
-        }
-        String wordSize = "";
-        while (!stringList.isEmpty()) {
-            if (!Objects.equals(stringList.get(0), "#")) {
-                wordSize += stringList.remove(0);
-            } else {
-                int size = Integer.parseInt(wordSize);
-                stringList.remove("#");
-                String word = "";
-                for (int j = 0; j < size; j++) {
-                    word += stringList.remove(0);
-                }
-                wordSize = "";
-                decodedString.add(word);
+        List<String> result = new ArrayList<>();
+        int i = 0;
+        while (i < str.length()) {
+            int j = i;
+            while (str.charAt(j) != '#') {
+                j += 1;
             }
+            int length = Integer.parseInt(str.substring(i, j));
+            result.add(str.substring(j + 1, j + 1 + length));
+            i = j + 1 + length;
         }
-        return decodedString;
+        return null;
     }
 
 }
